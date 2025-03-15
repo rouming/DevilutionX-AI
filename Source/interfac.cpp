aa54d809d33b9529aac8362cae92f7b91ed4c5a0
Roman Penyaev <r.peniaev@gmail.com>
2025-03-15 18:58:25 +0100

    diablo: split EventHandler on actual handler and poller callbacks
    There are various message handlers in the code, including:
    
      PollEvent()
      FetchMessage()
      FetchMessage_Real()
    
    These functions internally call SDL_PollEvent() and then handle the
    received events differently depending on the context.
    
    This commit separates the actual handlers from the poll invocations by
    adding an additional `poll` callback parameter. This parameter can be
    either SDL_PollEvent() or something else, allowing the caller of the
    message handler to decide which events need to be processed.
    
    This commit does not change the logic of event processing. Its main
    purpose is to make it possible to replace SDL_PollEvent() with
    SDL_PeepEvents() in the next patch.
    
    Signed-off-by: Roman Penyaev <r.peniaev@gmail.com>

--- Source/interfac.cpp
+++ Source/interfac.cpp
@@ -498,8 +498,8 @@ void ProgressEventHandler(const SDL_Event &event, uint16_t modState)
 		}
 
 		[[maybe_unused]] EventHandler prevHandler = SetEventHandler(ProgressEventHandlerState.prevHandler);
-		assert(prevHandler == ProgressEventHandler);
-		ProgressEventHandlerState.prevHandler = nullptr;
+		assert(prevHandler.handle == ProgressEventHandler);
+		ProgressEventHandlerState.prevHandler.handle = nullptr;
 		IsProgress = false;
 
 		Player &myPlayer = *MyPlayer;
@@ -554,7 +554,7 @@ void interface_msg_pump()
 {
 	SDL_Event event;
 	uint16_t modState;
-	while (FetchMessage(&event, &modState)) {
+	while (FetchMessage(&event, &modState, CurrentEventHandler.poll)) {
 		if (event.type != SDL_QUIT) {
 			HandleMessage(event, modState);
 		}
@@ -596,7 +596,8 @@ void ShowProgress(interface_mode uMsg)
 	gbSomebodyWonGameKludge = false;
 
 	ProgressEventHandlerState.loadStartedAt = SDL_GetTicks();
-	ProgressEventHandlerState.prevHandler = SetEventHandler(ProgressEventHandler);
+	EventHandler newHandler = { ProgressEventHandler, SDL_PollEvent };
+	ProgressEventHandlerState.prevHandler = SetEventHandler(newHandler);
 	ProgressEventHandlerState.skipRendering = true;
 	ProgressEventHandlerState.done = false;
 	ProgressEventHandlerState.drawnProgress = 0;
@@ -650,9 +651,9 @@ void ShowProgress(interface_mode uMsg)
 	while (true) {
 		CheckShouldSkipRendering();
 		SDL_Event event;
-		// We use the real `PollEvent` here instead of `FetchMessage`
+		// We use the real `PollEventCustom` here instead of `FetchMessage`
 		// to process real events rather than the recorded ones in demo mode.
-		while (PollEvent(&event)) {
+		while (PollEventCustom(&event, SDL_PollEvent)) {
 			if (!processEvent(event)) return;
 		}
 	}
