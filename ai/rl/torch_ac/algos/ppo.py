14a5972cd986941560e38145c684c145cc9bf235
Roman Penyaev <r.peniaev@gmail.com>
2025-10-03 20:19:42 +0200

    rc/torch_ac: do not calculate L2 norm for gradients manually
    The function `clip_grad_norm_` returns the total norm of the gradients
    before the clipping operation is performed, so it is numerically equal
    to the previous manual calculation.
    
    Signed-off-by: Roman Penyaev <r.peniaev@gmail.com>

--- ai/rl/torch_ac/algos/ppo.py
+++ ai/rl/torch_ac/algos/ppo.py
@@ -134,8 +134,7 @@ class PPOAlgo(BaseAlgo):
 
                 self.optimizer.zero_grad()
                 batch_loss.backward()
-                grad_norm = sum(p.grad.data.norm(2).item() ** 2 for p in self.acmodel.parameters()) ** 0.5
-                torch.nn.utils.clip_grad_norm_(self.acmodel.parameters(), self.max_grad_norm)
+                grad_norm = torch.nn.utils.clip_grad_norm_(self.acmodel.parameters(), self.max_grad_norm)
                 self.optimizer.step()
 
                 # Update log values
@@ -145,7 +144,7 @@ class PPOAlgo(BaseAlgo):
                 log_policy_losses.append(batch_policy_loss)
                 log_value_losses.append(batch_value_loss)
                 log_kls.append(batch_kl)
-                log_grad_norms.append(grad_norm)
+                log_grad_norms.append(grad_norm.item())
 
         # Log some values
 
