7e08a37c2d8acfd57d6832016a32803c75a838e6
Roman Penyaev <r.peniaev@gmail.com>
2026-01-05 13:38:48 +0100

    OLD BLOCKING RESET

--- ai/rl/torch_ac/utils/penv.py
+++ ai/rl/torch_ac/utils/penv.py
@@ -1,26 +1,22 @@
 import multiprocessing
-import selectors
-import numpy as np
+import gymnasium as gym
 
 
 def worker(conn, env):
     while True:
         cmd, data = conn.recv()
         if cmd == "step":
-            action, auto_reset = data
-            result = env.step(action)
-            if auto_reset:
-                obs, reward, terminated, truncated, info = result
-                if terminated or truncated:
-                    # Be careful here - the last observation is returned
-                    # right after reset, not the actual observation that
-                    # causes termination. This should not cause any
-                    # harm for training because the algorithm does not
-                    # actually use the next observation when done=True.
-                    # See @ParallelEnv.step()
-                    obs, reset_info = env.reset()
-                    info |= reset_info
-                    result = (obs, reward, terminated, truncated, info)
+            result = env.step(data)
+            terminated, truncated = result[2:4]
+            if terminated or truncated:
+                # Be careful here - the last observation is returned
+                # right after reset, not the actual observation that
+                # causes termination. This should not cause any
+                # harm for training because the algorithm does not
+                # actually use the next observation when done=True.
+                # See @ParallelEnv.step()
+                obs, _ = env.reset()
+                result = (obs,) + result[1:]
 
             conn.send(result)
         elif cmd == "reset":
@@ -33,14 +29,12 @@ def worker(conn, env):
 class ParallelEnvPool:
     def __init__(self, envs):
         assert len(envs) >= 1, "No environment given."
-        self.sel = selectors.DefaultSelector()
         self.ctx = multiprocessing.get_context('fork')
         self.envs = envs
-        self.conns = []
-        for i, env in enumerate(self.envs):
+        self.locals = []
+        for env in self.envs[1:]:
             local, remote = self.ctx.Pipe()
-            self.sel.register(local, selectors.EVENT_READ, i)
-            self.conns.append(local)
+            self.locals.append(local)
             p = self.ctx.Process(target=worker, args=(remote, env))
             p.daemon = True
             p.start()
@@ -52,103 +46,40 @@ class ParallelEnv:
 
     def __init__(self, penv_pool, auto_reset=False):
         self.p = penv_pool
-        self.auto_reset = auto_reset
-        self.inv = np.empty(len(self.p.envs), dtype=int)
 
-
-    def nonblock_reset(self, seeds=None, indices=None):
-        if seeds is None and indices is None:
-            seeds = [None] * len(self.p.envs)
-            indices = range(0, len(self.p.envs))
-        elif seeds is None and indices is not None:
-            assert len(indices)
-            seeds = [None] * len(indices)
-        elif seeds is not None and indices is None:
-            assert len(seeds) == len(self.p.envs)
-            indices = range(0, len(self.p.envs))
-        elif seeds is not None and indices is not None:
-            assert len(indices)
-            assert len(seeds) == len(indices)
-            assert len(indices) <= len(self.p.envs)
-        else:
-            assert 0, "Unknown seeds/indices combination"
-
-        for i, ind in enumerate(indices):
-            local, seed = self.p.conns[ind], seeds[i]
-            local.send(("reset", seed))
-
-        return len(indices)
-
-
-    def poll_resets(self, *, nonblock):
-        indices = []
-        results = []
-        timeout = 0 if nonblock else None
-        events = self.p.sel.select(timeout=timeout)
-        for e, mask in events:
-            i = e.data
-            conn = e.fileobj
-            result = conn.recv()
-            indices.append(i)
-            results.append(result)
-
-        obss, infos = zip(*results) if results else ((), ())
-        return indices, obss, infos
-
+        # For bot environment spaces are missing
+        if hasattr(self.p.envs[0], 'observation_space'):
+            self.observation_space = self.p.envs[0].observation_space
+        if hasattr(self.p.envs[0], 'action_space'):
+            self.action_space = self.p.envs[0].action_space
 
     def reset(self, seeds=None, indices=None):
-        nr_resets = self.nonblock_reset(seeds=seeds, indices=indices)
-
-        obss = np.empty(nr_resets, dtype=object)
-        infos = np.empty(nr_resets, dtype=object)
-
-        # Check if we do need the reverse mapping
-        if indices is None:
-            use_inverse = False
+        if seeds is not None:
+            assert len(seeds) and len(seeds) <= len(self.p.envs)
         else:
-            sched_indices = np.asarray(indices, dtype=int)
-            use_inverse = not np.all(sched_indices == np.arange(nr_resets))
+            seeds = [None] * len(self.p.envs)
 
-            if use_inverse:
-                self.inv[sched_indices] = np.arange(nr_resets)
+        for local, seed in zip(self.p.locals, seeds[1:]):
+            local.send(("reset", seed))
 
-        done = 0
-        while done < nr_resets:
-            ind, obs, info = self.poll_resets(nonblock=False)
-            ind = np.asarray(ind, dtype=int)
-
-            pos = self.inv[ind] if use_inverse else ind
-            obss[pos] = obs
-            infos[pos] = info
-            done += len(ind)
-
-        return obss.tolist(), infos.tolist()
-
-
-    def step(self, actions, indices=None):
-        if indices is None:
-            assert len(actions) == len(self.p.envs)
-            indices = range(0, len(self.p.envs))
-        else:
-            assert indices is not None and len(indices)
-            assert len(actions) == len(indices)
-            assert len(indices) <= len(self.p.envs)
-
-        for i, ind in enumerate(indices):
-            local, action = self.p.conns[ind], actions[i]
-            local.send(("step", (action, self.auto_reset)))
-
-        results = []
-        for i, ind in enumerate(indices):
-            local = self.p.conns[ind]
-            result = local.recv()
-            obs, reward, terminated, truncated, info = result
-
-            result = obs, reward, terminated, truncated, info
-            results.append(result)
-
+        results = [self.p.envs[0].reset(seed=seeds[0])] + \
+            [local.recv() for local, _ in zip(self.p.locals, seeds[1:])]
         return zip(*results)
 
+    def step(self, actions, active_indices=None):
+        assert len(actions) and len(actions) <= len(self.p.envs)
+
+        for local, action in zip(self.p.locals, actions[1:]):
+            local.send(("step", action))
+
+        result = self.p.envs[0].step(actions[0])
+        terminated, truncated = result[2:4]
+        if terminated or truncated:
+            # See the comment in @worker above
+            obs, _ = self.p.envs[0].reset()
+            result = (obs,) + result[1:]
+
+        return zip(*[result] + [local.recv() for local, _ in zip(self.p.locals, actions[1:])])
 
     def render(self):
         raise NotImplementedError
